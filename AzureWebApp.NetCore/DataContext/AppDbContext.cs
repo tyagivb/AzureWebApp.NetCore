5eedfe9cbb562a414d48068cddabf26e7d03c8c9
tyagivb <45883027+tyagivb@users.noreply.github.com>
2023-08-08 16:19:59 +0530

    Staging (#1)
    * commit code
    
    * Add or update the Azure App Service build and deployment workflow config
    
    * commit code
    
    ---------
    
    Co-authored-by: Vishal Tyagi <vishal.tyagi@hotmail.com>

---
+++ AzureWebApp.NetCore/DataContext/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace AzureWebApp.NetCore.DataContext
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
